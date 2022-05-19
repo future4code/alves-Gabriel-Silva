bf20ff609e7821fa00b31f6b9b896b338681c693
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-05-18 22:07:35 -0300

    exercício de laços incompleto

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,34 @@
+// #### Exercícios de interpretação de código:
+
+// exercício 1
+// resposta: o que entendi foi que, enquanto o i for menor que 5, lhe é acrescentado +1, e a let "valor" vai receber o valor de i a cada vez que o laço se repetir,
+//  sempre adicionando ao "valor" as somas obtidas anteriormente, até que o i seja falso, ou === 5.
+// OBS: passei tempinho legal imprimindo no python tutor para enteder o que estava acontecendo
+
+// ### exercício 2
+
+// a) O que vai ser impresso no console?
+// resposta:  será impresso todos os números maiores que 18.
+
+// resposta (b): consegui fazer a impressãp do índice do elemento usando apenas o for...
+
+// exercício 3: o resultado seriam 4 asterísticos "****". pois o laço só pararia de adicionar após o valor ou quantidade igualhar o número do prompt que foi 4.
+
+
+// #####  Exercícios de escrita de código
+
+
+// exercício 1: OBS: deu a hora do plantão e estava tentando montar essa estrutura, após o plantão tinha um compromisso, espero conseguir completar amanhã
+//  pela manhã cedo ou no decorrer do dia...
+
+// let quantBichos = Number(prompt("Digite quantos animais de estimação você possui:"))
+
+
+// if (quantBichos === 0) {
+//     console.log("Que pena! Você pode adotar um pet!")
+//     }else if(quantBichos > 0) {
+     
+//       while (quantBichos > 0)
+// }
+
+     
