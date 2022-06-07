797f504cc895d1e492f29123d104d24b68b71899
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-06-07 20:56:32 -0300

    exercicio props

---
+++ modulo2/props-labedin/labedin/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import imagemPerf from './img/imgPerfil.jpg'
+import CardPequeno from './components/CardPequeno/CardPequeno';
+import imgEmail from './img/email.webp'
+import imgEndereco from './img/endereco.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem={imagemPerf}
+          nome="Gabriel" 
+          descricao="Oi, eu sou Gabriel. Sou aluno da Labenu. Estou mudando de área proficional, anteriormente trabalhava na área do turismo e agora estou na jornada rumo ao desenvolvimento web full stack. "
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <CardPequeno
+        imgEmail={imgEmail}
+        imgendereco={imgEndereco}
+        email = 'Email: gabrieldos55570000@gmail.com'
+        endereco='Rio Formoso-PE'
+        
+         />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        />
+        
+        <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
