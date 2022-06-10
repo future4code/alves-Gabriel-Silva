f1bdeec39b1aedcc16bfab1e4aa7f0de1a3102a3
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-06-10 15:58:53 -0300

    ideia de estrutura

---
+++ modulo2/projeto-whatslab/src/App.js
@@ -0,0 +1,18 @@
+import logo from '';
+import '';
+
+function App() {
+  return (
+    <div className="App"> {/*usar width 40% center*/}
+      <header className="App-header">
+        <div>{/* ícone/foto de usuário + nome que ao clicar leva a mais informações + uma nave de opções*/}</div>
+      </header>
+      <body>
+        {/*fazer aparecer as mensagens enviadas*/}
+      </body>
+      <footer>{/*colocar dois inputs, um para envio do nome de usuário e outro input para a mensagem, e um button para envio de ambos */}</footer>
+    </div>
+  );
+}
+
+export default App;
