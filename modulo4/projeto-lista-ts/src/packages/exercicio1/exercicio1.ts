e3601ca180b7082ae8dec07efb10a9dcc81dbae1
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-08-14 22:40:40 -0300

    projeto-lista-ts

--- modulo4/projeto-lista-ts/src/packages/exercicio1/exercicio1.ts
+++ modulo4/projeto-lista-ts/src/packages/exercicio1/exercicio1.ts
@@ -4,23 +4,23 @@ const retornaFrase = (nome: string, nascimento: string): string => {
     const data = nascimento.split("/")
 
     switch (data[1]){
-        case "01" || "1":
+        case "01" :
             data[1] = "janeiro"
-        case "02" || "2":
+        case "02" :
             data[1] = "fevereiro"
-        case "03" || "3":
+        case "03" :
             data[1] = "março"
-        case "04" || "4":
+        case "04" :
             data[1] = "abril"
-        case "05" || "5":
+        case "05" :
             data[1] = "maio"
-        case "06" || "6":
+        case "06" :
             data[1] = "junho"
-        case "07" || "7":
+        case "07" :
             data[1] = "julho"
-        case "08" || "8":
+        case "08" :
             data[1] = "agosto"
-        case "09" || "9":
+        case "09" :
             data[1] = "setembro"
         case "10" :
             data[1] = "outubro"
@@ -35,4 +35,4 @@ const retornaFrase = (nome: string, nascimento: string): string => {
     const ano = data[2]
     return `Olá me chamo ${nome}, nasci no dia ${dia} do mês de ${mes} do ano de ${ano}.` 
 }
-console.log(process.argv[2])
+console.log(retornaFrase(process.argv[2], process.argv[3]))
