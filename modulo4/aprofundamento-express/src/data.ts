9d812d6fcaa09396f1af5b1c698458a3ea5a07e5
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-08-16 21:34:11 -0300

    exercício incompleto

---
+++ modulo4/aprofundamento-express/src/data.ts
@@ -0,0 +1,59 @@
+// Exercício 2
+
+type Afazeres = {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+
+// Exercício 3
+
+export const afazeres: Afazeres[] = [
+    {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false
+    },
+    {
+        userId: 1,
+        id: 2,
+        title: "et porro tempora",
+        completed: true
+    },
+    {
+        userId: 1,
+        id: 3,
+        title: "delectus aut autem",
+        completed: false
+    },
+    {
+        userId: 1,
+        id: 4,
+        title: "et porro tempora",
+        completed: true
+    }, {
+        userId: 1,
+        id: 5,
+        title: "delectus aut autem",
+        completed: false
+    },
+    {
+        userId: 1,
+        id: 6,
+        title: "et porro tempora",
+        completed: true
+    }, {
+        userId: 1,
+        id: 7,
+        title: "delectus aut autem",
+        completed: false
+    },
+    {
+        userId: 1,
+        id: 8,
+        title: "et porro tempora",
+        completed: true
+    }
+]
